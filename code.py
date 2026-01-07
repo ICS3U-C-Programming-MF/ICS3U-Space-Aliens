7df36d2a3a2b8401cf6e9c1fb701a9f5df833c75
Maximiliano Fairman <Maximiliano.fairman@stu.ocsb.ca>
2026-01-07 17:33:06 +0000

    code.py

--- code.py
+++ code.py
@@ -8,35 +8,54 @@ import stage
 
 
 def game_scene():
-    # main scene
+    # This function is the main scene of the game
 
-    # image banks
+    # image banks for circuit python
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # sets background to the image 0
+    # sets the background to the image 0 in the image bank
+    # and the size to 10x8 tiles of 16x16 pixels each
     background = stage.Grid(image_bank_background, 10, 8)
 
-    # a sprite will updated every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # a sprite for that will be updated every frame
+    cowboy = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-    # sets stage for the background
+    # create a stage for the game background
     game = stage.Stage(ugame.display, 60)
 
-    # background layer
-    game.layers = [ship] + [background]
+    # set the background layer
+    game.layers = [cowboy] + [background]
 
-    # all sprites
+    # render all sprites
     game.render_block()
 
-    # Loop forever
+    # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("START")
+        if keys & ugame.K_SELECT:
+            print("SELECT")
+        if keys & ugame.K_RIGHT:
+            cowboy.move(cowboy.x + 1, cowboy.y)
+        if keys & ugame.K_LEFT:
+            cowboy.move(cowboy.x - 1, cowboy.y)
+        if keys & ugame.K_UP:
+            cowboy.move(cowboy.x, cowboy.y - 1)
+        if keys & ugame.K_DOWN:
+            cowboy.move(cowboy.x, cowboy.y + 1)
 
         # update game logic
 
         # redraw Sprites
-        game.render_sprites([ship])
+        game.render_sprites([cowboy])
         game.tick()
 
 
