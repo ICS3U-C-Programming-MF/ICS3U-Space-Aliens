5134ade0e5f3095dff01f473d16787101c146e81
Maximiliano Fairman <Maximiliano.fairman@stu.ocsb.ca>
2026-01-07 17:02:00 +0000

    code.py

--- code.py
+++ code.py
@@ -1,16 +1,44 @@
+#!/usr/bin/env python3
+# created by maximiliano
+# date jan 07 2026
+# This program is the "space aliens program on the pybadge
+
 import ugame
 import stage
 
-def game_scene():
 
+def game_scene():
+    # main scene
+
+    # image banks
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # sets background to the image 0
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # a sprite will updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+    # sets stage for the background
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+
+    # background layer
+    game.layers = [ship] + [background]
+
+    # all sprites
     game.render_block()
+
+    # Loop forever
     while True:
-        pass
-    
-if __name__ == '__main__':
+        # get user input
+
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
+
+
+if __name__ == "__main__":
     game_scene()
--- unmaine_code.py
+++
@@ -1,3 +0,0 @@
-
-print ("hello shem!")
-print ("I LOVE PYTHON!")
